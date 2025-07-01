f853f11fee5bdb8be2fcca6f38ec79e4d519a601
jeetusingh247 <jeetusingh11010@gmail.com>
2025-07-01 22:08:36 +0530

    variables with different data types

--- 01_first_C_program/02_variables.c
+++ 01_first_C_program/02_variables.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main() {
 
@@ -17,14 +18,53 @@ int main() {
 
     // Float
 
-    float scgpa = 8.9;
-    float price = 19.99;
-    float temperature = -10.1;
+    // float scgpa = 8.9;
+    // float price = 19.99;
+    // float temperature = -10.1;
 
-    printf("Your scgpa is %.1f\n", scgpa); // C has default behaviour to show 6 digits after decimal point.
-    printf("the price is $%.2f\n", price); // 2 decimal point
-    printf("Temperature is %fCelcius\n", temperature);
+    // printf("Your scgpa is %.1f\n", scgpa); // C has default behaviour to show 6 digits after decimal point.
+    // printf("the price is $%.2f\n", price); // 2 decimal point
+    // printf("Temperature is %fCelcius\n", temperature);
 
+    // Double
+    // double pi = 3.1415939994859503; // 15-16 digits after decimal can be stored
+    // double e = 2.71828383939930;
+
+    // printf("The value of pi is %.15lf\n", pi); // format specifier is long float --> double
+    // printf("The value of e is %.15lf\n", e);
+
+    // character
+
+    // char grade = 'A';
+    // char symbol = '$';
+
+    // printf("Your Grade is %c\n", grade);
+    // printf("Symbol of money is %c\n", symbol);
+
+    // In C program we use array of char to represent a string.
+
+    // char name[] = "Jeetu Singh";
+    // char food[] = "Double Cheeze Burger";
+    // char email[] = "admin@gmail.com";
+
+    // printf("Hello %s\n", name);
+    // printf("Would you like to have %s ?\n", food); // %s to display string
+    // printf("Your Email : %s\n", email);
+
+    // Boolean --> data type
+    // bool isOnline = true; // 1
+    // bool canVote = false; // 0
+
+    // printf("Is he/she online : %d\n", isOnline); // --> 1
+    // printf("Can he/she vote : %d\n", canVote); // --> 0
+
+    // if (isOnline)
+    // {
+    //     printf("Yes You Can Connect !");
+    // } else
+    // {
+    //     printf("You cannot connect !");
+    // }
 
     return 0;
 }
