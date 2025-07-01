f8ee66c4ea1cb52e7472084d94e469821c21ab77
jeetusingh247 <jeetusingh11010@gmail.com>
2025-07-01 18:31:43 +0530

    my first C program

---
+++ 01_first_C_program/01_program.c
@@ -0,0 +1,16 @@
+#include <stdio.h> // preprocessor directive
+
+int main() { // main function --> entry point of program
+
+    // this is my first program ! :-)
+
+    /*
+    I am using multi-line comments
+    here in C programming
+    */
+
+    printf("Vantara is Amazing !\n");
+    printf("Hub for Endangered Species");
+
+    return 0; 
+}
