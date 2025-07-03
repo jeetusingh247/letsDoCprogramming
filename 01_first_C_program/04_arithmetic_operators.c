70326a0a3ebe1c97a722e4b47e49a78e43db4ba6
jeetusingh247 <jeetusingh11010@gmail.com>
2025-07-03 20:39:27 +0530

    arithmetic operators

---
+++ 01_first_C_program/04_arithmetic_operators.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+
+    // arithmetic operators = + - * / % ++ --
+
+    int x = 10;
+    int y = 2;
+    int z = 0;
+
+    // Arithmetic Operator
+
+    // z = x + y; // 5
+    // z = x - y; // -1
+    // z = x * y; // 6
+    // z = x / y;
+    // z = x % y; // rem--> 0
+
+    // Augmented Assignment Operator
+
+    // x = x + 2;
+    // x += 2;
+    // x *= 2;
+    // x = x * 2;
+    // x = x / 2;
+    // x /= 2;
+
+    printf("%d\n", x);
+}
